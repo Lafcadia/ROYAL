934fabca6ae3f5f96905c00404f70e15d5cad52f
ChuishenX <chuishenx@oblivionocean.top>
2024-07-19 19:42:10 +0800

    Upgrade

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 
 import openai
+from openai import OpenAI
 
-openai.api_base = 'https://api.aigcapi.io/v1'
-openai.api_key = os.getenv("AIGCAPI_KEY")
+client = OpenAI(api_key=os.getenv("AIGCAPI_KEY"))
+
+# TODO: The 'openai.api_base' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(base_url='https://api.aigcapi.io/v1')'
+# openai.api_base = 'https://api.aigcapi.io/v1'
 
 messages = [
     {"role": "user", "content": "你好"}
 ]
 
-res = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=messages,
-    stream=False,
-)
+res = client.chat.completions.create(model="gpt-4",
+messages=messages,
+stream=False)
 
-print(res['choices'][0]['message']['content'])
+print(res.choices[0].message.content)
